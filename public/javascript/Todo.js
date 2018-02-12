bade7c903c2fcd2e9ce642b3a95297b671cf9fff
Nate Sage <davidsage@Davids-MacBook-Pro.local>
2018-02-11 17:29:36 -0800

    This is better

--- public/javascript/Todo.js
+++ public/javascript/Todo.js
@@ -10,36 +10,36 @@ function myTodoFunction() {
     var listItemsValue = document.querySelector('#userInputText').value;
 
     if (listItemsValue.length > 0) {
-    //pushs the value of the listItems array
-    listItems.push(listItemsValue);
+        //pushs the value of the listItems array
+        listItems.push(listItemsValue);
 
-    //creates a span, a new list item, and a button into the the created div
-    divAndFuckBoys.innerHTML =
-    `
+        //creates a span, a new list item, and a button into the the created div
+        divAndFuckBoys.innerHTML =
+            `
     <span class="addACheck">✅</span> ${listItems} <p class="delete">Delete</p>
     `;
-    renderStuff.appendChild(divAndFuckBoys)
-    document.querySelector('#userInputText').value = "";
-
-} else {};
-    divAndFuckBoys.addEventListener("click", function () {
-        //reverses the value of the variable to be true first then keeps reversing as the function runs
-        isCompleted = !isCompleted;
-
-        //removes the line through the new list item when first rendered to the created div
-        if (isCompleted === true) {
-            divAndFuckBoys.classList.remove("todoCompleted");
-        }
-        //adds a css class element to put a line through the list item when checkButton is clicked
-        else {
-            divAndFuckBoys.classList.add("todoCompleted");
-        };
-    });
-    //removes the element in the row when the deleteButton is clicked
-    divAndFuckBoys.querySelector(".delete").addEventListener("click", function () {
-        renderStuff.removeChild(divAndFuckBoys);
-    });
+        renderStuff.appendChild(divAndFuckBoys)
+        document.querySelector('#userInputText').value = "";
 
 
+        divAndFuckBoys.addEventListener("click", function () {
+            //reverses the value of the variable to be true first then keeps reversing as the function runs
+            isCompleted = !isCompleted;
+
+            //removes the line through the new list item when first rendered to the created div
+            if (isCompleted === true) {
+                divAndFuckBoys.classList.remove("todoCompleted");
+            }
+            //adds a css class element to put a line through the list item when checkButton is clicked
+            else {
+                divAndFuckBoys.classList.add("todoCompleted");
+            };
+        });
+        //removes the element in the row when the deleteButton is clicked
+        divAndFuckBoys.querySelector(".delete").addEventListener("click", function () {
+            renderStuff.removeChild(divAndFuckBoys);
+        });
+
+    }
 };
 
